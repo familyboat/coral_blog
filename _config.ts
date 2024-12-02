07ddb2b06caf169750bbcd3d97c403db1e2afcaf
familyboat <2015301110129@whu.edu.cn>
2024-12-02 17:09:13 +0800

    localize

--- _config.ts
+++ _config.ts
@@ -1,9 +1,16 @@
 import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
+import { zhCN } from "date-fns/locale";
 
 const site = lume();
 
-site.use(blog());
+site.use(blog({
+  date: {
+    locales: {
+      zhCN
+    }
+  }
+}));
 site.ignore("./posts/instructions.md");
 
 export default site;
